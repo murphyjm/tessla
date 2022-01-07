157b6f361250fb9b939ce7f4f5a53a17be044033
Joey Murphy <joseph.murphy@ucsc.edu>
2022-01-07 09:37:15 -0800

    Added an extra test. Now on to the photometry modeling logic

--- test/test_get_photometry.py
+++ test/test_get_photometry.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 from tessla.tesssystem import TessSystem
 
 def test_downlaod_phot_simple():
@@ -20,6 +21,9 @@ def test_downlaod_phot_simple():
     assert lc.time[break_ind + 1] - lc.time[break_ind] > 10, "Something went wrong with the data download."
 
 def test_normalized_flux():
+    '''
+    Check that the normalization seems to work properly.
+    '''
     toi = TessSystem('HIP8152', 
                     tic=164767175, 
                     toi=266,
@@ -30,3 +34,13 @@ def test_normalized_flux():
                     verbose=True)
     lc = toi.get_tess_phot()
     assert np.abs(np.median(lc.flux) - 1) < 0.1, "Something is wrong with the flux normalization."
+
+def test_no_data_available():
+    '''
+    Try to download data for a bogus TIC number.
+    '''
+    foo = TessSystem('Foobar', 
+                    tic=1234567890,
+                    toi=123)
+    with pytest.raises(AssertionError):
+        foo.get_tess_phot()
