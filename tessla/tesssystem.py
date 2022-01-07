ad791aedc5a2bb5566c78a850ace22f6f60fe185
Joey Murphy <joseph.murphy@ucsc.edu>
2022-01-06 18:38:01 -0800

    Downloading and cleaning/normalizing the data correctly

--- tessla/tesssystem.py
+++ tessla/tesssystem.py
@@ -1,4 +1,6 @@
+from re import search
 from astroquery.utils import download_file_list
+from lightkurve import lightcurve
 import numpy as np
 import pandas as pd
 import lightkurve as lk
@@ -11,105 +13,94 @@ class TessSystem:
                 name, # CPS ID or common name if not in Jump.
                 tic=None, 
                 toi=None, 
-                author=None, # SPOC or QLP
-                sectors=np.array([], dtype=int), # Sectors you'd like to include. Can also pass string 'all' to use all available sectors.
+                mission='TESS', # Only use TESS data by default. Could also specify other missions like "Kepler" or "all".
                 cadence=120, # By default, extract the 2-minute cadence data, as opposed to the 20 s cadence, if both are available for a TOI.
-                default_flux_origin='pdcsap_flux', # By default, try to use the PDCSAP flux. If this isn't available for all sectors, the SAP flux will be used for consistency.
-                ntransiting=1) -> None: # Annotation denotes that __init__ should always return None
+                flux_origin='sap_flux', # By default, use the SAP flux. Can also specify "pdcsap_flux" but this may not be available for all sectors.
+                ntransiting=1,
+                verbose=True) -> None:
         self.name = name
         self.tic = tic
         self.toi = toi
-        self.author = author
+        self.mission = mission
         self.cadence = cadence
         self.ntransiting = ntransiting
+        self.verbose = verbose
 
-        # Private attributes
-        self._all_sectors_flag = False # Want to download all available sectors of data? Underscore at start since this attribute isn't necessarily needed by the client.
-        self._default_flux_origin = default_flux_origin
+       # Default flux origin to use. SAP flux used by default.
+        self.flux_origin = flux_origin
 
-        # Handle which sectors the user wants to use.
-        # NOTE: Easier/cleaner way to do this?
-        if type(sectors) == str:
-            if sectors.lower() == 'all':
-                self._all_sectors_flag = True
-        if not self._all_sectors_flag:
-            try:
-                len(sectors)
-            except TypeError:
-                sectors = np.array([sectors], dtype=int) # If only a single sector passed as an int or float, then make it a single-element array.
-            self.sectors = sectors
-
-    def get_tess_phot(self) -> pd.DataFrame:
+    def get_tess_phot(self) -> lk.LightCurve:
         '''
         Download the TESS photometry. 
         
-        Use PDCSAP flux by default, but can specify to use SAP flux instead.
-        If multiple sectors of photometry are requested and one sector has e.g. SAP flux but no PDCSAP flux, 
-        then use SAP flux for all sectors, even if PDCSAP is available for some, for consistency. An example of this is TOI-1824, 
-        which has 30-min cadence Sector 15 SAP light curve, but no PDCSAP light curve for Sector 15 (as opposed to its other sectors,
-        which are 2-min cadence and have both PDCSAP and SAP.) 
+        Use SAP flux by default, but can specify to use PDCSAP flux instead, 
+            though PDCSAP flux might not be available for all sectors.
         '''
-        lc_dict = self.__download_tess_phot(self)
-        assert None not in lc_dict.values(), "Problem downloading the TESS photometry."
-        flux_origins = [lc.FLUX_ORIGIN for lc in lc_dict.values()]
-        if all(flux_origins == 'pdcsap_flux'):
-            # If PDCSAP flux is available for all sectors, the use whatever the user requested as the flux type. 
-            # Could have a scenario where PDCSAP is available for all sectors but the user specifies to use SAP flux instead. 
-            # This is fine because if SAP flux should be available regardless of whether PDCSAP is available or not. 
-            flux_origin = self._default_flux_origin
-        elif any(flux_origins == 'sap_flux'):
-            # If PDCSAP flux is *not* available for all sectors e.g. for TOI-1824, then use SAP flux for all sectors for consistency.
-            flux_origin = 'sap_flux'
-            if self._default_flux_origin != 'sap_flux':
-                # If the user requested to use PDCSAP flux for all sectors but this is not possible, let them know about the change.
-                print(f"Requested to use {self._default_flux_origin} but this is not available for all sectors.\n Using {flux_origin} instead for consistency.")
+        # Download the photometry
+        collection = self.__download_tess_phot()
         
-        # Add this attribute to the TessSystem object specifying whether we're using the PDCSAP or SAP light curve.
-        self.flux_origin = flux_origin
+        # Clean and normalize the photometry 
+        lc = self.__clean_and_normalize_tess_phot(collection)
 
-        self.__clean_and_normalize_tess_photometry(self, lc_dict)
-
-        return self.lc_df
+        self.lc = lc
+        return lc
         
-    def __download_tess_phot(self) -> dict:
+    def __download_tess_phot(self) -> lk.LightCurveCollection:
         '''
-        Download the lightkurve objects, if available. 
-        Private method, since the user shouldn't need to access this one specifically.
-        '''
-        lc_dict = {}
-        for sector in self.sectors:
-            print(f"Downloading Sector {sector} data...")
-            success = False
-            for author in ['SPOC', 'QLP']:
-                if not success:
-                    lc = lk.search_lightcurve(f"TIC {self.tic}", author=author, sector=sector)
-                if not lc:
-                    print(f"No {author} light curve found for Sector {sector}...")
-                else:
-                    lc = lc.download()
-                    lc_dict[sector] = lc
-                    success = True
-                    print(f"Download successful for Sector {sector} {author} light curve...")
-        return lc_dict
+        Download the photometry.
 
-    def __clean_and_normalize_tess_photometry(self, lc_dict) -> None:
+        Only use lightcurves collected with the same exposure time. 
+            I.e., don't combine a sector of 30-min cadence FFI data with a 2-min cadence light curve. 
+            Could adjust this in the future, but strict use of same cadence data for now.
+
+        Returns: The collection of lk.Light
+        '''
+        search_result = lk.search_lightcurve(f"TIC {self.tic}")
+        if self.verbose:
+            print(f"Found {len(search_result)} data products for TIC {self.tic}:")
+            print(search_result)
+            print(f"Extracting all {self.cadence} s cadence data...")
+        
+        # Pick out the data of the correct cadence and mission. Could update both of these options later.
+        # E.g. at the moment, you can't ask for data from both Kepler and TESS, and/or 2-min data and 30-min data.
+        mask = search_result.exptime.value == self.cadence # Only use data with the same exposure time. Could change this if wanted.
+        mission_series = pd.Series(search_result.mission)
+        mask &= mission_series.str.contains(self.mission, case=False, regex=False).values # Only use data from the same mission.
+        assert np.sum(mask) > 0, "No data fits the cadence and mission criteria for this target."
+
+        # Isolate the lightkurve objects that we want based on the mask above and download the data.
+        collection = search_result[mask].download_all()
+        assert collection is not None, "Trouble downloading the data"
+        return collection
+
+    def __stitch_corrector(self, lc) -> lk.lightcurve:
+        '''
+        Corrector function to be passed to lk.LightCurveCollection.stitch().
+        '''
+        # Clean the data
+        lc = lc.remove_nans().normalize().remove_outliers()
+        lc = lc[lc.quality == 0]
+
+        # Normalize the flux
+        flux = getattr(lc, self.flux_origin)
+        flux_normed = (flux.value / np.median(flux.value) - 1) * 1e3 # Puts flux in units of PPT
+        flux_err = lc.flux_err.value / np.median(flux.value) * 1e3   # Puts flux error in units of PPT
+
+        # Add these columns to the lightcurve object.
+        lc['norm_flux'] = flux_normed # PPT
+        lc['norm_flux_err'] = flux_err # PPT
+
+        return lc
+
+    def __clean_and_normalize_tess_phot(self, collection) -> lk.LightCurve:
         '''
         Do some initial cleaning and normalizing of the TESS photometry.
-        '''
-        df = pd.DataFrame([], columns=['time', 'flux', 'flux_err'])
 
-        # Iterate over each sector of photometry.
-        for lc in lc_dict.values():
-            lc = lc.remove_nans().normalize().remove_outliers()
-            lc = lc[lc.quality == 0]
-            flux = getattr(lc, self.flux_origin)
-            flux_normed = (flux.value / np.median(flux.value) - 1) * 1e3 # Puts flux in units of PPT
-            flux_err = lc.flux_err.value / np.median(flux.value) * 1e3   # Puts flux error in units of PPT
-            lc_sector_df = pd.DataFram(data={'time':lc.time.value, 'flux':flux_normed, 'flux_err':flux_err})
-            df = pd.concat((df, lc_sector_df))
-        
-        # Add the concatenated dataframe containing the light curve for all sectors as an attribute.
-        self.lc_df = df
+        Args
+        -----
+        collection (lk.LightCurveCollection): Light curve collection containing the data we'll use.
+        '''
+        return collection.stitch(self.__stitch_corrector)
 
     def get_rvs(self, rv_fname):
         pass
--- test/test_get_photometry.py
+++ test/test_get_photometry.py
@@ -1,8 +1,32 @@
-import os
-import pytest
-from tessla import tesssystem
+import numpy as np
+from tessla.tesssystem import TessSystem
 
-def test_downlaod_photometry():
+def test_downlaod_phot_simple():
     '''
-    
+    Simple test to see if downloading the photometry works properly.
     '''
+    toi = TessSystem('HIP8152', 
+                    tic=164767175, 
+                    toi=266,
+                    mission="TESS",
+                    cadence=120,
+                    flux_origin='sap_flux', 
+                    ntransiting=2, 
+                    verbose=True)
+    lc = toi.get_tess_phot()
+    break_ind = np.argmax(np.ediff1d(lc.time))
+
+    # There should be sectors of data spaced by a wide time gap.
+    assert lc.time[break_ind + 1] - lc.time[break_ind] > 10, "Something went wrong with the data download."
+
+def test_normalized_flux():
+    toi = TessSystem('HIP8152', 
+                    tic=164767175, 
+                    toi=266,
+                    mission="TESS",
+                    cadence=120,
+                    flux_origin='sap_flux', 
+                    ntransiting=2, 
+                    verbose=True)
+    lc = toi.get_tess_phot()
+    assert np.abs(np.median(lc.flux) - 1) < 0.1, "Something is wrong with the flux normalization."
