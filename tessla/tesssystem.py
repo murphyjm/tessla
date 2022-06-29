d3dc600647e7faba30336d05314f9e1195fd8a9c
Joey Murphy <joseph.murphy@ucsc.edu>
2022-06-29 14:39:41 -0700

    Trying to add prior on tau where lower bound is value of rho

--- tessla/tesssystem.py
+++ tessla/tesssystem.py
@@ -628,13 +628,13 @@ class TessSystem:
         '''
         Create an exponentially-decaying SHOTerm GP kernel.
         '''
-        log_sigma_dec_gp = pm.Normal("log_sigma_dec_gp", mu=0., sigma=10)
+        log_sigma_dec_phot_gp = pm.Normal("log_sigma_phot_gp", mu=0., sigma=10)
         BoundedNormal = pm.Bound(pm.Normal, lower=np.log(1), upper=np.log(50)) # Bounded normal for the periodic length scale so it's forced to be longer than 12 hours so it doesn't interfere with transit fitting. 
-        log_rho_gp = BoundedNormal("log_rho_gp", mu=np.log(10), sd=np.log(50))
-        BoundedNormalTau = pm.Bound(pm.Normal, lower=np.log(1), upper=np.log(200))
-        log_tau_gp = BoundedNormalTau("log_tau_gp", mu=np.log(10), sd=np.log(50))
-        kernel = terms.SHOTerm(sigma=tt.exp(log_sigma_dec_gp), rho=tt.exp(log_rho_gp), tau=tt.exp(log_tau_gp))
-        noise_params = [log_sigma_dec_gp, log_rho_gp, log_tau_gp]
+        log_rho_phot_gp = BoundedNormal("log_rho_phot_gp", mu=np.log(10), sd=np.log(50))
+        BoundedNormalTau = pm.Bound(pm.Normal, lower=log_rho_phot_gp, upper=np.log(200))
+        log_tau_phot_gp = BoundedNormalTau("log_tau_phot_gp", mu=np.log(10), sd=np.log(50))
+        kernel = terms.SHOTerm(sigma=tt.exp(log_sigma_dec_phot_gp), rho=tt.exp(log_rho_phot_gp), tau=tt.exp(log_tau_phot_gp))
+        noise_params = [log_sigma_dec_phot_gp, log_rho_phot_gp, log_tau_phot_gp]
         return noise_params, kernel
 
     def __get_rotation_kernel(self, suffix=''):
@@ -998,14 +998,15 @@ class TessSystem:
             if self.include_svalue_gp:
                 # These parameters shared by all GPs
                 gp_svalue_params = []
-                BoundedNormalProt = pm.Bound(pm.Normal, lower=np.log(1), upper=np.log(50))
                 if self.svalue_gp_kernel == 'rotation':
+                    BoundedNormalProt = pm.Bound(pm.Normal, lower=np.log(1), upper=np.log(50))
                     log_prot_rv_gp = BoundedNormalProt("log_prot_rv_gp", mu=np.log(self.rot_per), sd=np.log(5)) # self.rot_per from LS periodogram of OoT flux but can be superseded
                     prot_rv_gp = pm.Deterministic("prot_rv_gp", tt.exp(log_prot_rv_gp))
                     gp_svalue_params += [log_prot_rv_gp]
                 elif self.svalue_gp_kernel == 'exp_decay':
-                    log_rho_svalue_gp = BoundedNormalProt("log_rho_svalue_gp", mu=np.log(10), sd=np.log(50)) # Maybe change this to be informed from the periodogram of the photometry.
-                    BoundedNormalTau = pm.Bound(pm.Normal, lower=np.log(1), upper=np.log(200))
+                    BoundedNormalRho = pm.Bound(pm.Normal, lower=np.log(1), upper=np.log(50))
+                    log_rho_svalue_gp = BoundedNormalRho("log_rho_svalue_gp", mu=np.log(10), sd=np.log(50)) # Maybe change this to be informed from the periodogram of the photometry.
+                    BoundedNormalTau = pm.Bound(pm.Normal, lower=log_rho_svalue_gp, upper=np.log(200))
                     log_tau_svalue_gp = BoundedNormalTau("log_tau_svalue_gp", mu=np.log(10), sd=np.log(50)) # Maybe change this to be seeded with longer period.
                     gp_svalue_params += [log_rho_svalue_gp, log_tau_svalue_gp]
                 
