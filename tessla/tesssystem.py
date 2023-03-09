ad96d5074b1f27148789bf1ba856c0bb267b80a2
Joey Murphy <joseph.murphy@ucsc.edu>
2023-03-09 11:39:03 -0800

    Extended range of gamma_rv uniform to +/-250 m/s from 100 m/s

--- tessla/tesssystem.py
+++ tessla/tesssystem.py
@@ -976,7 +976,7 @@ class TessSystem:
             for tel in self.rv_inst_names:
                 mask = self.rv_df['tel'] == tel
                 gamma_rv_list.append(np.median(self.rv_df.loc[mask, 'mnvel']))
-            gamma_rv = pm.Uniform("gamma_rv", lower=-100, upper=100, shape=self.num_rv_inst)
+            gamma_rv = pm.Uniform("gamma_rv", lower=-250, upper=250, shape=self.num_rv_inst)
             
             BoundedNormalSigmaRV = pm.Bound(pm.Normal, lower=np.log(0.1), upper=np.log(20))
             log_sigma_rv_mu = np.empty(len(self.rv_inst_names))
